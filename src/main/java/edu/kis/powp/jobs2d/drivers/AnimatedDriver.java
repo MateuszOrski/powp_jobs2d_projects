1cc2d8a4a228ae6a61c05aab8bfc0d0b9819eb38
Jakub Lewandowski <lewando54@gmail.com>
2026-01-09 17:56:18 +0100

    feature: added AnimatedDriver

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/AnimatedDriver.java
@@ -0,0 +1,66 @@
+package edu.kis.powp.jobs2d.drivers;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import javax.swing.SwingUtilities;
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class AnimatedDriver implements Job2dDriver {
+    private static final int DEFAULT_DELAY_MS = 100;
+    
+    private final Job2dDriver targetDriver;
+    private final int delayMs;
+    private final BlockingQueue<Runnable> operationQueue;
+    private final Thread executionThread;
+    private volatile boolean running = true;
+
+    public AnimatedDriver(Job2dDriver targetDriver) {
+        this(targetDriver, DEFAULT_DELAY_MS);
+    }
+
+    public AnimatedDriver(Job2dDriver targetDriver, int delayMs) {
+        this.targetDriver = targetDriver;
+        this.delayMs = delayMs;
+        this.operationQueue = new LinkedBlockingQueue<>();
+        this.executionThread = new Thread(this::processOperations);
+        this.executionThread.setDaemon(true);
+        this.executionThread.start();
+    }
+
+    @Override
+    public void setPosition(int x, int y) {
+        queueOperation(() -> targetDriver.setPosition(x, y));
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+        queueOperation(() -> targetDriver.operateTo(x, y));
+    }
+
+    private void queueOperation(Runnable operation) {
+        try {
+            operationQueue.put(operation);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void processOperations() {
+        while (running) {
+            try {
+                Runnable operation = operationQueue.take();
+                Thread.sleep(delayMs);
+                SwingUtilities.invokeLater(operation);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Animated " + targetDriver.toString();
+    }
+}
+
